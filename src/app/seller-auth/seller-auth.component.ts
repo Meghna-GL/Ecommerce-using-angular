0b0479e071b18ddf11c4f1105b70e237604abb83
Meghna-GL <meghna.srivastava@globallogic.com>
2023-01-04 18:33:28 +0530

    feat: Seller signup API integration completed

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-//import { SellerService } from '../services/seller.service';
-
+import { SellerService } from '../services/seller.service';
+import { Router } from '@angular/router';
+import { SignUp } from '../data-type';
 @Component({
   selector: 'app-seller-auth',
   templateUrl: './seller-auth.component.html',
@@ -8,13 +9,16 @@ import { Component } from '@angular/core';
 })
 export class SellerAuthComponent {
 
-  // constructor(private seller:SellerService){
+  constructor(private seller:SellerService, private router:Router){
 
-  // }
+  }
 
 
-  signUp(data:object):void{
-    console.warn(data)
-    //this.seller.userSignUp(); 
+  signUp(data:SignUp):void{
+    this.seller.userSignUp(data).subscribe((result)=>{
+      if(result){
+        this.router.navigate(['seller-home'])
+      }
+    }); 
   }
 }
